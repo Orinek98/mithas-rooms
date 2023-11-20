a46914249055a0019b445906e6a2818d27bb00c2
Orinek98 <saveriosantonocito@outlook.com>
2023-11-20 19:07:35 +0100

    added type ref

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,12 +1,13 @@
 import { getAllRooms } from "@/lib/rooms";
+import type { dbRooms } from "@/lib/rooms";
 
 export default async function RoomsPage() {
     const data = await getAllRooms();
 
   return (
     <div>
-        {data.map((room) =>(
-            <div key={room.id}>
+        {data.map((room: dbRooms ) =>(
+            <div key={room.id as string}>
                 {room.name}
             </div>
         )
