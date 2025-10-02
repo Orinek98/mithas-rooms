576d2673de14bc822c91672aa393a48b431cfbfb
Saverio Santonocito <119307808+Orinek98@users.noreply.github.com>
2025-10-02 09:40:14 +0200

    Update route.ts

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -11,7 +11,7 @@ export async function POST(request : Request){
         host: "localhost",
         database: "hotel",
         user: "root",
-        password: "ghepardo98"
+        password: "##########"
 
     })
 
